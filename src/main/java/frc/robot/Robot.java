fcf9575d9729fc3316112528297764013b13f0f3
Hayden Mitchell <hayd6262@gmail.com>
2026-02-05 21:00:03 -0500

    Initial Commit
    Includes feeder and launcher

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,122 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import java.util.List;
+
+import com.ctre.phoenix6.SignalLogger;
+import com.revrobotics.util.StatusLogger;
+
+import edu.wpi.first.epilogue.Epilogue;
+import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.epilogue.NotLogged;
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.Drivetrain;
+
+@Logged
+public class Robot extends TimedRobot {
+  private Command m_autonomousCommand;
+
+  public Drivetrain drivetrain;
+  public Controls controls;
+
+  private List<String> autoNames;
+  private SendableChooser<String> autoChooser;
+  private String lastSelectedAuto;
+
+
+  public Robot() {
+    SignalLogger.start();
+    DataLogManager.start();
+    DriverStation.startDataLog(DataLogManager.getLog());
+    StatusLogger.start();
+    Epilogue.bind(this);
+    
+    controls = Controls.getInstance();
+    drivetrain = Drivetrain.getInstance();
+  }
+  
+  public static boolean isPracticeBot() {
+    return false;
+  }
+
+  @Override
+  public void robotInit() {
+  }
+
+  @Override
+  public void robotPeriodic() {
+    CommandScheduler.getInstance().run();
+  }
+
+
+  public double getTime() {
+    return DriverStation.getMatchTime();
+  }
+
+  @Override
+  public void disabledInit() {
+  }
+
+  @Override
+  public void disabledPeriodic() {
+  }
+
+  @Override
+  public void disabledExit() {
+  }
+
+  @Override
+  public void autonomousInit() {
+    if (m_autonomousCommand != null) {
+      CommandScheduler.getInstance().schedule(m_autonomousCommand);
+    }
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+  }
+
+  @Override
+  public void autonomousExit() {
+  }
+
+  @Override
+  public void teleopInit() {
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
+  }
+
+  @Override
+  public void teleopPeriodic() {
+  }
+
+  @Override
+  public void teleopExit() {
+  }
+
+  @Override
+  public void testInit() {
+    CommandScheduler.getInstance().cancelAll();
+  }
+
+  @Override
+  public void testPeriodic() {
+  }
+
+  @Override
+  public void testExit() {
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Launcher.java
@@ -0,0 +1,66 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import static frc.robot.Constants.LauncherConstants.*;
+
+public class Launcher extends SubsystemBase {
+
+  TalonFX leftUp, leftLow, rightUp, rightLow;
+
+  /** Creates a new Launcher. */
+  public Launcher() {
+    leftUp = new TalonFX(leftUpID);
+    leftLow = new TalonFX(leftLowID);
+    rightUp = new TalonFX(rightUpID);
+    rightLow = new TalonFX(rightLowID);
+
+     var config = new TalonFXConfiguration();
+    config.Slot0.kP = kP;
+    config.Slot0.kI = kI;
+    config.Slot0.kD = kD;
+    config.Slot0.kV = kV;
+    config.Slot0.kS = kS;
+    config.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+    config.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+    config.CurrentLimits.StatorCurrentLimit = currentLimit;
+    config.CurrentLimits.StatorCurrentLimitEnable = true;
+    leftUp.getConfigurator().apply(config);
+    leftUp.getVelocity().setUpdateFrequency(50); // Set update frequency to 50 Hert, 20ms
+    leftUp.getClosedLoopError().setUpdateFrequency(50);
+    leftUp.getClosedLoopOutput().setUpdateFrequency(50);
+    leftUp.getSupplyVoltage().setUpdateFrequency(20);
+    leftUp.getSupplyCurrent().setUpdateFrequency(20);
+    leftUp.getStatorCurrent().setUpdateFrequency(20);
+    leftUp.optimizeBusUtilization();
+
+    config.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+    rightUp.getConfigurator().apply(config);
+    rightUp.getVelocity().setUpdateFrequency(50); // Set update frequency to 50 Hert, 20ms
+    rightUp.getClosedLoopError().setUpdateFrequency(50);
+    rightUp.getClosedLoopOutput().setUpdateFrequency(50);
+    rightUp.getSupplyVoltage().setUpdateFrequency(20);
+    rightUp.getSupplyCurrent().setUpdateFrequency(20);
+    rightUp.getStatorCurrent().setUpdateFrequency(20);
+    rightUp.optimizeBusUtilization();
+
+    leftLow.setControl(new Follower(leftUp.getDeviceID(), MotorAlignmentValue.Aligned));
+    rightLow.setControl(new Follower(rightUp.getDeviceID(), MotorAlignmentValue.Aligned));
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
