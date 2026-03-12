a5173f9b2d4cc817b6c9ea90a250b458ef36402e
Kevin-OConnor <koconnor@usfirst.org>
2026-03-11 22:43:27 -0400

    Add separate sequence for vision launching

--- src/main/java/frc/robot/commands/CalcLaunchSequence.java
+++ src/main/java/frc/robot/commands/CalcLaunchSequence.java
@@ -9,13 +9,13 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class LaunchSequence extends SequentialCommandGroup {
+public class CalcLaunchSequence extends SequentialCommandGroup {
   /** Creates a new LaunchSequence. */
 
-  public LaunchSequence(double launchSpeed) {
+  public CalcLaunchSequence() {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
-    addCommands(new SpinUp(launchSpeed), new Launch(launchSpeed));
+    addCommands(new CalcVisionSpinUp(), new CalcVisionLaunchSpeed());
     
   }
 }
