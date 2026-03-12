a6703bde57ca8f801f880aa1d1d1aaef426dcf68
Robert Durell <rdurell@gmail.com>
2026-03-11 21:57:20 -0400

    Commands for getting launch speed in dashboard

---
+++ src/main/java/frc/robot/commands/LaunchSequenceDashboard.java
@@ -0,0 +1,27 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class LaunchSequenceDashboard extends SequentialCommandGroup {
+  /** Creates a new LaunchSequence. */
+
+  public LaunchSequenceDashboard() {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+
+    SmartDashboard.putNumber("nearShot", Constants.LauncherConstants.launchSpeedNear);
+    addCommands(new SpinUpDashboard(),
+        new Launch(Constants.LauncherConstants.launchSpeedNear)); // Import CalcVisionSpinUp + CalcLaunchSpeed?
+
+  }
+
+}
---
+++ src/main/java/frc/robot/commands/SpinUpDashboard.java
@@ -0,0 +1,51 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.LauncherConstants;
+import frc.robot.subsystems.Launcher;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class SpinUpDashboard extends Command {
+  /** Creates a new SpinUp. */
+
+  public SpinUpDashboard() {
+    
+    addRequirements(Launcher.getInstance());
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+
+    double launchSpeed = SmartDashboard.getNumber("nearShot", LauncherConstants.launchSpeedNear);
+    Launcher.getInstance().setSpeed(launchSpeed);
+    Launcher.getInstance().setCommand("SpinUpDashboard");
+  } 
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    if (interrupted) {
+      Launcher.getInstance().stop();
+    }
+    Launcher.getInstance().setCommand("");
+
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return Launcher.getInstance().atSpeed();
+  }
+}
+ 
